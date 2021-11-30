959714d45b1bbd65cccc66f8fedc799e78cdda12
pro100DeSkree <deskree4@gmail.com>
2021-11-30 19:16:20 +0200

    Commit

--- Translate/main.py
+++ Translate/main.py
@@ -1,5 +1,5 @@
 from PyQt5 import QtWidgets
-from TranslateUI import Ui_MainWindow  # импорт нашего сгенерированного файла
+from TranslateUI import Ui_MainWindow  # импорт UI файла
 import sys
 
 
@@ -8,20 +8,12 @@ class mywindow(QtWidgets.QMainWindow):
         super(mywindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        #layout = QtWidgets.QGridLayout()
-        #self.setLayout(layout)
 
         self.ui.LineTranslate_1.setMaxLength(34)                  # Ограничение символов в поле ввода 1
         self.ui.LineTranslate_2.setMaxLength(34)                  # Ограничение символов в поле ввода 2
         self.ui.Translate.clicked.connect(self.TranslateClicked)  # Подключение клик сигнала к слоту TranslateClicked
         self.ui.ASK_Button.clicked.connect(self.ASKClicked)       # Подключение клик сигнала к слоту ASKClicked
-        #self.ui.ASK_CheckBox.toggle()
-        #self.ui.ASK_CheckBox.stateChanged.connect(self.changeTitle)
-        #self.ui.ASK_CheckBox.toggled.connect(self.changeTitle)
-        #layout.addWidget(self.ui.ASK_CheckBox, 0, 0)
-
-    def changeTitle(self):
-        print("hz kak")
+        self.ui.ASK_CheckBox.isTriState(self.changeTitle)
 
     def ASKClicked(self):
         print("Clicked ASK")
