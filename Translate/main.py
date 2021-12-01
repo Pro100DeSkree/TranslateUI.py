b5f0382753737e8b8fba8932faaf66d77c98c365
pro100DeSkree <deskree4@gmail.com>
2021-12-01 20:07:44 +0200

    Commit

--- Translate/main.py
+++ Translate/main.py
@@ -3,6 +3,8 @@ from TranslateUI import Ui_TranslateAPP  # импорт UI файла
 from googletrans import Translator                       # Импортируем гугл переводчик
 import sys
 
+Boxlang1 = ["English", "Russian", "Ukraine"]           # Список языков в QComboBox1
+Boxlang2 = ["Russian", "English", "Ukraine"]           # Список языков в QComboBox2
 
 class mywindow(QtWidgets.QMainWindow):
     # Settings
@@ -13,22 +15,43 @@ class mywindow(QtWidgets.QMainWindow):
         self.ui.setupUi(self)
         self.ui.LineTranslate_1.setMaxLength(34)                       # Ограничение символов в поле ввода 1
         self.ui.LineTranslate_2.setMaxLength(34)                       # Ограничение символов в поле ввода 2
-        self.ui.Translate.clicked.connect(self.TranslateClicked)       # Подключение клик сигнала к def TranslateClicked
+        self.ui.Translate.clicked.connect(self.CheckLangBoxes)       # Подключение клик сигнала к def TranslateClicked
         self.ui.ASK_Button.clicked.connect(self.ASKClicked)            # Подключение клик сигнала к def ASKClicked
         self.ui.ASK_CheckBox.stateChanged.connect(self.Check_Answer)   # Подключение "клик" сигнала к def Check_Answer
         self.ui.TimeSpinBox.valueChanged.connect(self.spinboxChanged)  # Подключение спинбокса к def spinboxChanged
-        self.ui.Languages_1.addItems(["Ubuntu", "Mandriva",            # Список языков в QComboBox
-                                      "Fedora", "Arch", "Gentoo"])
-        self.ui.Languages_2.addItems(["Ubuntu", "Mandriva",            # Список языков в QComboBox
-                                      "Fedora", "Arch", "Gentoo"])
-        self.ui.Languages_1.activated[str].connect(self.Lang_ComboBox_1)  # Отправка данных из QComboBox в функцию
-        self.ui.Languages_2.activated[str].connect(self.Lang_ComboBox_2)  # Отправка данных из QComboBox в функцию
+        self.ui.Languages_1.addItems(Boxlang1)                            # Список языков в QComboBox1
+        self.ui.Languages_2.addItems(Boxlang2)                            # Список языков в QComboBox2
 
     # Function
-    def TranslateClicked(self):                                        # Функция реагирования на клик
+    def translate(self, lang1, lang2):                          # Функция реагирования на клик
+
+        print(lang1, lang2)
+
         word = self.ui.LineTranslate_1.text()                          # Вывод содержимого поля 1
-        translate = self.translator.translate(word, dest='ru')
-        self.ui.LineTranslate_2.setText(translate.text)
+        #translate = self.translator.translate(word, src=lang1, dest=lang2)
+        #self.ui.LineTranslate_2.setText(translate.text)
+
+    def CheckLangBoxes(self):
+        lang1 = self.ui.Languages_1.currentText()
+        lang2 = self.ui.Languages_2.currentText()
+
+        # lang1_1 = ""
+        # lang1_2 = ""
+        if lang1 == "English":
+            lang1_1 = 'en'
+        elif lang1 == "Russian":
+            lang1_1 = 'ru'
+        else:
+            lang1_1 = 'uk'
+
+        if lang2 == "English":
+            lang1_2 = 'en'
+        elif lang2 == "Russian":
+            lang1_2 = 'ru'
+        else:
+            lang1_2 = 'uk'
+
+        self.translate(lang1_1, lang1_2)
 
     def ASKClicked(self):
         print("Clicked ASK")
@@ -41,12 +64,6 @@ class mywindow(QtWidgets.QMainWindow):
 
     def spinboxChanged(self, value):
         print('New value of spinbox is:', value)
-
-    def Lang_ComboBox_1(self, text):
-        print(text)
-
-    def Lang_ComboBox_2(self, text):
-        print(text)
 
     # Initialization
 app = QtWidgets.QApplication([])
